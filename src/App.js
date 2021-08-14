9650bf95f932b1448b233364784ecf77b638032f
edwinnv <edwinjunior2000@outlook.com>
2021-08-13 19:12:29 -0500

    Add About

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ const App = () => {
   const [pokemon, setPokemon] = useState("pikachu");
   const [hp, sethp]= useState(" ");
   const [attack, setattack]= useState(" ");
-  const [id, setid] = useState(" ");
+  
   const [defense, setdefense]= useState(" ");
+  const [about, setabout] = useState(" ");
   const [pokemonData, setPokemonData] = useState([]);
-  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonType, setPokemonType] = useState(""); 
 
   const handleChange = (e) => {
     setPokemon(e.target.value.toLowerCase());
@@ -27,27 +28,41 @@ const App = () => {
       const res = await axios.get(url);
       toArray.push(res.data);
       setPokemonType(res.data.types[0].type.name);
-      setid(res.data.id);
       setPokemonData(toArray);
       sethp(res.data.stats[0].base_stat);
       setattack(res.data.stats[1].base_stat);
       setdefense(res.data.stats[2].base_stat);
+      getPokemon1(res.data.id);
       
     } catch (e) {
       console.log(e);
     }
   };
 
-  console.log(pokemonData);
+  const getPokemon1 = async (id) => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/ability/${id}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setabout(res.data.effect_entries[1].effect);
+      
+      
+    } catch (e) {
+      console.log(e);
+    }
+  };
+   console.log(about);
+
  
-  console.log(id);
+ 
   return (
 
 
     <div className="App p-4" >  
       <form onSubmit={handleSubmit}>
         <label>
-        <div class="mb-3 fs-3 fw-semi">PokeSearch</div>
+        <div className="mb-3 fs-3 fw-semi">PokeSearch</div>
           <input
             type="text"
             onChange={handleChange}
@@ -64,10 +79,10 @@ const App = () => {
               <div className="">No.{data.id} {data.name}</div>
               <div className="divTable">
                 <div className="divTableBody">              
-                  {/* <div className="divTableRow">
+                   <div className="divTableRow">
                     <div className="divTableCell">Tipo</div>
                     <div className="divTableCell">{pokemonType}</div>
-                  </div> */}
+                  </div> 
                   <div className="divTableRow">
                     <div className="divTableCell">HP</div>
                     <div className="divTableCell"> {hp}                   
@@ -82,6 +97,10 @@ const App = () => {
                   <div className="divTableRow">
                     <div className="divTableCell">Defense</div>
                     <div className="divTableCell">{defense}</div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">About</div>
+                    <div className="divTableCell">{about}</div>
                   </div>
                 </div>                     
               </div>
