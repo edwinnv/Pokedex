93b5b63160d6d5483c03f0d49326a650b246a9fb
edwinnv <edwinjunior2000@outlook.com>
2021-08-19 15:50:30 -0500

    Update 3:50 19/08/2021

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import styled from "styled-components";
+
 
 
 const myinput = {
   position: "absolute",
-  left: "35.38%",
+  left: "34.38%",
   right: "34.38%",
-  top: "12.8%",
+  top: "15.8%",
   bottom: "72.17%",
-  fontsize: "6px",
-  fontfamily: "Poppins",
-  fontstyle: "normal",
+
 
   boxsizing: "border-box",
   size: "2rem",
@@ -24,12 +22,14 @@ const myinput = {
 
 const mycard = {
   /* Card */
-  position: "absolute",
+  position: "center",
   left: "23.54%",
   right: "23.33%",
   top: "13.28%",
   bottom: "16.5%",
-  
+
+  width: "765px",
+  height: "719px",
 
   background: " #FFFFFF",
   boxshadow: "0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2)"
@@ -38,9 +38,9 @@ const mycard = {
 const pokeshearch = {
   /* PokeSearch */
   position: "absolute",
-  left: "42.15%",
+  left: "44.15%",
   right: "42.15%",
-  top: "6.26%",
+  top: "10.26%",
   bottom: "80.18%",
 
   fontfamily: "Poppins",
@@ -54,171 +54,284 @@ const pokeshearch = {
 
 const pokeimg = {
   position: "absolute",
- right: "40%",
-  width: "134px",
-  height: "130px",
+  right: "44.20%",
+  height: "180px",
+  width: "180px",
+
   top: "25.26%"
 }
 
 const namepoke = {
   /* No 1 Bulbasaur */
-position: "absolute",
-left: "45.01%",
-right: "41.81%",
-top: "40.29%",
-bottom: "48.14%",
-width: "600px",
-fontfamily:"Poppins",
-fontstyle: "normal",
-fontweight: "600",
-fontsize: "30px",
-lineheight: "45px",
-texttransform: "capitalize",
+  position: "absolute",
+  left: "45.30%",
+  right: "41.81%",
+  top: "40.29%",
+  bottom: "48.14%",
+  width: "600px",
+  fontfamily: "Poppins",
+  fontstyle: "normal",
+  fontweight: "600",
+  fontsize: "30px",
+  lineheight: "45px",
+  texttransform: "capitalize",
 
-color:"#414141"
+  color: "#414141"
 }
 const about1 = {
   /* About */
-position: "absolute",
-left: "24.93%",
-right: "68.75%",
-top: "45.07%",
-bottom: "45.41%",
-fontfamily: "Poppins",
-fontstyle: "normal",
-fontweight: "500",
-fontsize: "24px",
-lineheight: "36px",
-/* identical to box height */
+  position: "absolute",
+  left: "32.50%",
+  right: "68.75%",
+  top: "45.07%",
+  bottom: "45.41%",
+  fontfamily: "Poppins",
+  fontstyle: "normal",
+  fontweight: "500",
+  fontsize: "24px",
+  lineheight: "36px",
+  /* identical to box height */
 
-color: "#414141"
+  color: "#414141"
 }
 const abouttext = {
   position: "absolute",
-left: "24.93%",
-right: "24.72%",
-top: "54.79%",
-bottom: "35.45%",
+  left: "32.50%",
+  right: "24.72%",
+  top: "51.79%",
+  bottom: "35.45%",
 
-fontfamily: "Open Sans",
-fontstyle: "normal",
-fontweight: "normal",
-fontsize: "18px",
-lineheight: "25px",
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "12px",
+  lineheight: "25px",
 
-color: "#5D5D5D"
+  width: "600px",
+
+  color: "#5D5D5D"
 }
 
 const types1 = {
   /* Types */
-position: "absolute",
-left: "24.93%",
-right: "68.03%",
-top: "70.46%",
-bottom: "28.03%",
-fontfamily: "Poppins",
-fontstyle: "normal",
-fontweight: "500",
-fontsize: "24px",
-lineheight: "36px",
-/* identical to box height */
-color: "#414141"
+  position: "absolute",
+  left: "32.50%",
+  right: "68.03%",
+  top: "68.46%",
+  bottom: "28.03%",
+  fontfamily: "Poppins",
+  fontstyle: "normal",
+  fontweight: "500",
+  fontsize: "24px",
+  lineheight: "36px",
+  /* identical to box height */
+  color: "#414141"
 }
 
 const typetext = {
   /* Grass, Poisson */
-position: "absolute",
-left: "24.93%",
-right: "63.54%",
-top: "78.17%",
-bottom: "25.39%",
-fontfamily: "Open Sans",
-fontstyle: "normal",
-fontweight: "normal",
-fontsize: "18px",
-lineheight: "25px",
-/* identical to box height */
-color:"#5D5D5D"
+  position: "absolute",
+  left: "33.00%",
+  right: "63.54%",
+  top: "74.17%",
+  bottom: "25.39%",
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "18px",
+  lineheight: "25px",
+  /* identical to box height */
+  color: "#5D5D5D"
 }
 
 const stats1 = {
   /* Stats */
-position: "absolute",
-left: "75.07%",
-right: "33.07%",
-top: "70.46%",
-bottom: "28.03%",
+  position: "absolute",
+  left: "55.07%",
+  right: "33.07%",
+  top: "68.46%",
+  bottom: "28.03%",
 
-fontfamily: "Poppins",
-fontstyle: "normal",
-fontweight: "500",
-fontsize: "24px",
-lineheight: "36px",
-/* identical to box height */
-color: "#414141"
+  fontfamily: "Poppins",
+  fontstyle: "normal",
+  fontweight: "500",
+  fontsize: "24px",
+  lineheight: "36px",
+  /* identical to box height */
+  color: "#414141"
 
 }
 
 const stats2 = {
   /* Stats */
-position: "absolute",
-left: "72.07%",
-right: "36.29%",
-top: "74.97%",
-bottom: "26.17%",
+  position: "absolute",
+  left: "55.07%",
+  right: "36.29%",
+  top: "71.50%",
+  bottom: "26.17%",
 
-fontfamily: "Open Sans",
-fontstyle: "normal",
-fontweight: "bold",
-fontsize: "14px",
-lineheight: "19px",
-/* identical to box height */
-color: "#5D5D5D"
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "bold",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+  color: "#5D5D5D"
 
 }
 
-const value1 ={
+const value1 = {
   /* Value */
 
 
-position: "absolute",
-left: "85.15%",
-right: "24.72%",
-top: "74.97%",
-bottom: "26.17%",
+  position: "absolute",
+  left: "64.15%",
+  right: "24.72%",
+  top: "71.50%",
+  bottom: "26.17%",
 
-fontfamily: "Open Sans",
-fontstyle: "normal",
-fontweight: "bold",
-fontsize: "14px",
-lineheight: "19px",
-/* identical to box height */
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "bold",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
 
-textalign: "right",
+  textalign: "right",
 
-color: "#5D5D5D"
+  color: "#5D5D5D"
 }
 
 const text1 = {
   position: "absolute",
-left: "34.44%",
-right: "34.38%",
-top: "105.65%",
-bottom: "3.42%",
+  left: "39.44%",
+  right: "34.38%",
+  top: "92.65%",
+  bottom: "3.42%",
 
-fontfamily: "Open Sans",
-fontstyle: "normal",
-fontweight: "normal",
-fontsize: "18px",
-lineheight: "30px",
-/* identical to box height, or 167% */
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "18px",
+  lineheight: "30px",
+  /* identical to box height, or 167% */
 
-display: "flex",
-alignitems: "center",
+  display: "flex",
+  alignitems: "center",
 
-color: "#414141"
+  color: "#414141"
 
 }
+const hp1 = {
+  position: "absolute",
+  left: "55.07%",
+  right: "36.29%",
+  top: "73.83%",
+  bottom: "24.32%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+
+  color: "#5D5D5D"
+
+}
+const hp2 = {
+  position: "absolute",
+  left: "64.15%",
+  right: "24.72%",
+  top: "76.30%",
+  bottom: "24.32%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+  textalign: "right",
+
+  color: "#5D5D5D"
+}
+
+const attack1 = {
+  position: "absolute",
+  left: "55.07%",
+  right: "36.29%",
+  top: "75.80%",
+  bottom: "22.46%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+
+  color: "#5D5D5D"
+}
+
+const attack2 = {
+  position: "absolute",
+  left: "64.15%",
+  right: "24.72%",
+  top: "78.30%",
+  bottom: "22.46%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+  textalign: "right",
+
+  color: "#5D5D5D"
+
+}
+const defense1 = {
+  position: "absolute",
+  left: "55.07%",
+  right: "34.72%",
+  top: "78.00%",
+  bottom: "20.61%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+  color: "#5D5D5D"
+
+}
+const defense3 = {
+  position: "absolute",
+  left: "64.15%",
+  right: "24.72%",
+  top: "80.54%",
+  bottom: "20.61%",
+
+  fontfamily: "Open Sans",
+  fontstyle: "normal",
+  fontweight: "normal",
+  fontsize: "14px",
+  lineheight: "19px",
+  /* identical to box height */
+
+  textalign: "right",
+
+  color: "#5D5D5D"
+}
+
 
 
 
@@ -281,69 +394,49 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <label>
           <h1 style={pokeshearch}>PokeSearch</h1>
+
           <input
             type="text"
-            name= "ftext"
-            id= "ftext"
+            name="ftext"
+            id="ftext"
             style={myinput}
             onChange={handleChange}
             placeholder="Digite nombre Pokemon"
+
           />
+
+
         </label>
       </form>
 
-      {/* <ul>{pokemonData}</ul> */}
-      {/* <p>{[pokemonData]}</p> */}
 
       {pokemonData.map((data) => {
         return (
           <div className="container">
-            <img  alt="Pokemon" style={pokeimg} src={data.sprites["front_default"]} />
+            <img alt="Pokemon" style={pokeimg} src={data.sprites["front_default"]} />
             <h2 style={namepoke}>No {data.id} {data.name}</h2>
-            <h2 style ={about1}> About </h2>
+            <h2 style={about1}> About </h2>
             <div style={abouttext} >{about} </div>
-            <h2 style= {types1}> Types</h2>
-            <div style = {typetext}> {pokemonType} </div>
-            <h2 style = {stats1}> Stats </h2>
-            <h4 style= {stats2}> Stats </h4>
-            <h4 style = {value1}> Value</h4>
-            
-           {/*  <div className="divTable">
-              <div className="divTableBody">
-                <div className="divTableRow">
-                  <div className="divTableCell">Tipo</div>
-                  <div className="divTableCell">{pokemonType}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableCell">HP</div>
-                  <div className="divTableCell"> {hp}
-                  </div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableCell">Attack</div>
-                  <div className="divTableCell"> {attack}
+            <h2 style={types1}> Types</h2>
+            <div style={typetext}> {pokemonType} </div>
+            <h2 style={stats1}> Stats </h2>
+            <h4 style={stats2}> Stats </h4>
+            <h4 style={value1}> Value</h4>
+            <h4 style={hp1}>HP</h4>
+            <div style={hp2}> {hp}</div>
+            <h4 style={attack1}>Attack</h4>
+            <div style={attack2}>{attack}</div>
+            <h4 style={defense1}>Defense</h4>
+            <div style={defense3}>{defense}</div>
 
-                  </div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableCell">Defense</div>
-                  <div className="divTableCell">{defense}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableCell">About</div>
-                  <div className="divTableCell">{about}</div>
-                </div>
-              </div>
-            </div>  */}
-            
           </div>
-        
+
         );
-        
+
       })}
       <label style={text1} >  Copyright © 2021, Guarapo Labs | All rights reserved. </label>
     </div>
-    
+
 
   );
 };
